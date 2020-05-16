d86253ec2cc1410e0e6b557f4395daa2db0a7135
Jr08151 <Jr08151@users.noreply.github.com>
2020-05-15 21:01:05 -0400

    updated codes

--- challenges/classes.js
+++ challenges/classes.js
@@ -44,6 +44,7 @@
         width: 5,
         height: 5
     });
+    
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
